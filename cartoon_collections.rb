fcd50a922c8adb9254b98d84a42d2eb5d6a0fde6
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-31 21:34:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,11 @@ def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
   
-  array.map do |element|
-    element * element
-  end
+  # array.map do |element|
+  #   element * element
+  # end
+  
+  array.map { |elem| elem * elem }
   
 end
 
@@ -12,7 +14,9 @@ def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
   
-  planeteer_calls.map {|element| "#{element.capitalize}!"}
+  # planeteer_calls.map {|element| "#{element.capitalize}!"}
+  
+  planeteer_calls.map { |call| call.capitalize + '!' }
   
 end
 
@@ -29,23 +33,26 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
 
-  # index = 0 
-  # while index < valid_calls.length do
-  #   if planeteer_calls.find {|element| element == valid_calls[index]}
-  #     return valid_calls[index]
+  # 1 index = 0 
+  #   while index < valid_calls.length do
+  #     if planeteer_calls.find {|element| element ==     valid_calls[index]}
+  #       return valid_calls[index]
+  #     end
+  #   index += 1 
   #   end
-  # index += 1 
-  # end
 
 ### use if statement because this is AoA so we don't want to just return the result in first element after we need to return the result after finish checking all elements in array after
   
-  valid_calls.each do |result|
+  #2  valid_calls.each do |result|
     
-    if planeteer_calls.find {|element| element == result}
-      return result
-    end
+  #     if planeteer_calls.find {|element| element ==     result}
+  #       return result
+  #     end
     
-  end
+  #   end
+
+  #   nil
   
-  nil
+  planeteer_calls.find {|call| valid_calls.include?(call)}
+  
 end
